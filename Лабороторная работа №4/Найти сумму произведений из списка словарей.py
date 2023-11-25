c8354bfc61af956f5910b4871e6ad99d43ade9af
stch1p <149321582+stch1p@users.noreply.github.com>
2023-11-25 23:32:28 +0300

    Update Найти сумму произведений из списка словарей.py

--- Лабороторная работа №4/Найти сумму произведений из списка словарей.py
+++ Лабороторная работа №4/Найти сумму произведений из списка словарей.py
@@ -4,7 +4,7 @@ analytical_program= "input.json"
 def task() -> float:
     with open(analytical_program) as f:
         data = json.load(f)
-        list_values =[item["score"] * item["weight"] for item in data]
+        list_values = [item["score"] * item["weight"] for item in data]
         return round(sum(list_values), 3)
         
 print(task())
